2d38d92c496c74b7e861b1b9b51f75d2cec886ae
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-03 15:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
   return Object.assign({}, driver, {[key]: value})
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key) {
-
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  return driver
 }
